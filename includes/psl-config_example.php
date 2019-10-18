af176b6e42e33f39472d7e93a9af3f83713c4e78
Andrei Bubeneck <andrei.bubeneck@yahoo.com>
2019-10-18 18:40:40 +0200

    Added psl-config example

---
+++ includes/psl-config_example.php
@@ -0,0 +1,21 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * COMMON DATABASE FOR PARENT PAGE AND ALL MODULES
+ * These are the database login details defined as constants at runtime
+ */  
+define("HOST", "localhost");     // The host you want to connect to.
+define("USER", "username");    // The database username.
+define("PASSWORD", "password");    // The database password.
+define("DATABASE", "db name");    // The database name.
+
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);
